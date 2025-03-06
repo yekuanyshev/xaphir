bd7ea88aaa16d6b6394353c0daa6881e1cce8aca
yekuanyshev <kuanysheveldar123@gmail.com>
2025-03-06 11:45:41 +0500

    only 'ctrl+c' for tea.Quit

--- internal/tui/components/main.go
+++ internal/tui/components/main.go
@@ -33,7 +33,7 @@ func (m *Main) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			return m, tea.Quit
 		}
 	case tea.WindowSizeMsg:
